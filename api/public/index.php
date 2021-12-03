07ebe3a393960bcd3e22c0168f962d4ead28f67f
木瓜 <9664049+muguagua333@user.noreply.gitee.com>
2021-12-03 11:33:28 +0800

    v1.0.1

--- api/public/index.php
+++ api/public/index.php
@@ -17,7 +17,7 @@ namespace think;
 require __DIR__ . '/../vendor/autoload.php';
 
 // 定义程序当前版本号
-define('VAE_VERSION','1.0.0');
+define('VAE_VERSION','1.0.1');
 
 // 定义vaeThink项目目录
 define('VAE_ROOT', __DIR__ . '/../');
